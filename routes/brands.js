c39a5f92d0a04a4253c394228566fa3059fee22d
Gaurav Dhingra <gaurav@organizedoutdoor.com>
2020-05-19 09:14:22 +0530

    removed routes folder

--- routes/brands.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-// var helpers = require("../helpers/todos");
-
-router.get('/',function(req,res){
-    res.send("hello from todos route");
-});
-
-module.exports = router;
